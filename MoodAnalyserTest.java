86c30d6be66207e3aa9558a905841f6c7955f812
kanchan-solanke <kanchanssolanke@gmail.com>
2021-08-04 07:29:45 +0530

    when mood is null

--- MoodAnalyserTest.java
+++ MoodAnalyserTest.java
@@ -7,9 +7,24 @@ public class MoodAnalyserTest {
     private Assertions Assert;
 
     @Test
+    public void givenMessgae_WhenSad_ShouldReturn_Sad() {
+        MoodAnalyser moodAnalyzer = new MoodAnalyser("This is a Sad Message");
+        String mood = moodAnalyzer.analyseMood();
+        Assert.assertEquals("SAD",mood);
+
+    }
+
+    @Test
+    public void givenMessage_WhenNotSad_ShouldReturn_Happy() {
+        MoodAnalyser moodAnalyzer = new MoodAnalyser("This is Happy Messgae");
+        String mood = moodAnalyzer.analyseMood();
+        Assert.assertEquals("HAPPY",mood);
+    }
+
+    @Test
     public void givenNullMood_ShouldReturn_Happy() {
-        MoodAnalyser moodAnalyser = new MoodAnalyser(null);
-        String mood = moodAnalyser.analyseMood();
+        MoodAnalyser moodAnalyzer = new MoodAnalyser(null);
+        String mood = moodAnalyzer.analyseMood();
         Assert.assertEquals("HAPPY",mood);
     }
 }
