f605cd583bd46baf5d10060cbf40e877d78766cd
kanchan-solanke <kanchanssolanke@gmail.com>
2021-08-04 14:43:26 +0530

    I am in a sad mood return Sad

--- MoodAnalyser.java
+++ MoodAnalyser.java
@@ -1,7 +1,14 @@
 package org.example;
 
-public class MoodAnalyser {
-    public String analyseMood(String message) {
+public class MoodAnalyser
+{
+    public String message;
+    public MoodAnalyser(String msg)
+    {
+        message=msg;
+    }
+    public String analyseMood()
+    {
         if (message.contains("SAD"))
             return "SAD";
         else
